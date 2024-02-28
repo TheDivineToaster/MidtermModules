25940a48fba813afe37fd79d3d90d2db75b3e0a0
Micah <micahlee1577@gmail.com>
2024-02-27 20:18:04 -0600

    add color to notes

---
+++ exercise1/Notes/SharedResources.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes
+{
+    static class SharedResources
+    {
+        public static readonly Color FontColor = Color.FromRgb(0, 0, 0xFF);
+        public static readonly Color BackgroundColor = Color.FromRgb(0xB9, 0xDF, 0xC3);
+    }
+}
